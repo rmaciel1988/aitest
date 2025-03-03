3b5df63e9c435b53c88f82b85ed346892091137b
Reinaldo Maciel <maciel.oliveira.reinaldo@gmail.com>
2025-03-03 11:47:43 -0300

    reinaldo change

--- script/main.py
+++ script/main.py
@@ -1,3 +1,6 @@
+import re
+from collections import Counter
+
 def analyze_logs(log_file_path, top_responses=3, top_errors=2):
     """
     Analyze log file and generate a structured report.
@@ -12,6 +15,61 @@ def analyze_logs(log_file_path, top_responses=3, top_errors=2):
              top AI responses, and most common errors
     """
     # TODO: Implement this function
+
+    logs = read_file(log_file_path)
+    
+    # type count
+    pattern = r"\] (\w+) -"
+    message_types = re.findall(pattern, logs)
+    type_count = Counter(message_types)
+
+    # most used phrases
+    pattern = r"- (.+)"
+    messages = re.findall(pattern, logs)
+    normalized_messages = [msg.strip().lower().replace('"', '') for msg in messages]
+    phrases_count = Counter(normalized_messages)
+
+    # most common errors:
+    pattern = r"\] (\w+) - (.+)"
+
+    matches = re.findall(pattern, logs)
+
+    error_msg = []
+
+    for msg_type, text in matches:
+        text = text.strip().lower().replace('"', '')
+        if msg_type == 'ERROR':
+            error_msg.append(text)
+
+    error_counts = Counter(error_msg)
+
+    print(f'Total of errors {error_counts}')
+
+
+
+
+    '''
+    Log Summary:
+    - INFO messages: 42
+    - ERROR messages: 8
+    - WARNING messages: 5
+
+    Top 3 AI Responses:
+    1. "Hello! How can I help you today?" (12 times)
+    2. "I'm sorry, I didn't understand that." (7 times)
+    3. "Please provide more details." (5 times)
+
+    Most Common Errors:
+    1. Model Timeout after 5000ms (3 times)
+    2. API Connection Failure (2 times)
+    '''
+
+
+
+
+
+
+
     return read_file(log_file_path)
 
 def read_file(file_path):
